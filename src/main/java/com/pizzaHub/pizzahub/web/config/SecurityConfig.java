bd6971e9bc524be89ae545260b3b36faae824086
John Steven Bernal Gonzalez <bernalgonzalezjohn@gmail.com>
2023-12-04 21:17:06 -0500

    apply requestMatchers

--- src/main/java/com/pizzaHub/pizzahub/web/config/SecurityConfig.java
+++ src/main/java/com/pizzaHub/pizzahub/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.pizzaHub.pizzahub.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +16,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .requestMatchers(HttpMethod.PUT).denyAll()
                 .anyRequest()
                 .authenticated()
                 .and()
